08bf41deb90ebc4cf8c12aa72a658e12fc220bc2
mindimedia <bdm@mindimedia.com>
2024-12-17 02:51:36 +0800

    Update PORT

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,10 +3,6 @@ module.exports = {
         {
             name: 'Namatota',
             port: '4001',
-            exec_mode: 'fork',
-            instances: 4,
-            script: 'build/index.js',
-            max_memory_restart: '4G'
         }
     ]
 }
