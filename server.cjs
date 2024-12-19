f1661b61c5ed58e592d2bb8c851f2ee23fbe149a
mindimedia <bdm@mindimedia.com>
2024-12-19 19:01:23 +0800

    Update Build

--- server.cjs
+++ server.cjs
@@ -2,7 +2,7 @@ const express = require('express');
 const { handler } = require('./build/index.js');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4001;
 
 app.use(handler);
 
