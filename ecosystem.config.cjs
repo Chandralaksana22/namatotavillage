e85b90d3b3507c5e90439bb84691f88b6bf9db01
mindimedia <bdm@mindimedia.com>
2024-12-25 00:37:12 +0800

    Update Config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
         {
             name: 'Namatota',
             port: '4001',
+            autorestart: true,
+            cwd: ' /home/tisielmy/domains/namatota.my.id/public_html',
+            watch: false,
+            max_memory_restart: '200M',
         }
     ]
 }
