eddcd24d969d266ade49787ecaba7d7332e70c34
mindimedia <bdm@mindimedia.com>
2025-01-14 10:25:07 +0800

    Update Form

--- src/lib/component/section/Section-tour.svelte
+++ src/lib/component/section/Section-tour.svelte
@@ -12,118 +12,143 @@
 				<div class="section-title">Paket <span>Wisata</span></div>
 			</div>
 		</div>
-	{#if data?.length > 0}
-	<div class="row">
-		<div class="col-md-8">
-			<div class="item" style="height: 95%;">
-				<div class="position-re o-hidden" style="height: 100%;">
-					<img style="height: 100%;" src={data[0]?.images[0]?.path} alt="images" />
+		{#if data?.length > 0}
+			<div class="row">
+				<div class="col-md-8">
+					<div class="item" style="height: 95%;">
+						<div class="position-re o-hidden" style="height: 100%;">
+							<img style="height: 100%;" src={data[0]?.images[0]?.path} alt="images" />
+						</div>
+						<span class="category"
+							><a data-sveltekit-reload href="/paket-wisata/{data[0]?.slug}">{data[0]?.rate}</a
+							></span
+						>
+						<div class="con">
+							<h5>
+								<a data-sveltekit-reload href="/paket-wisata/{data[0]?.slug}">{data[0]?.name}</a>
+							</h5>
+							<div class="line"></div>
+							<div class="row facilities">
+								<div class="col col-md-12">
+									<ul>
+										<li><i class="ti-time"></i> {data[0]?.duration}</li>
+										<li><i class="ti-user"></i> {data[0]?.occupancy}</li>
+										<li><i class="ti-location-pin"></i> {data[0]?.location}</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-				<span class="category"><a data-sveltekit-reload href="/paket-wisata/{data[0]?.slug}">{data[0]?.rate}</a></span>
-				<div class="con">
-					<h5><a data-sveltekit-reload href="/paket-wisata/{data[0]?.slug}">{data[0]?.name}</a></h5>
-					<div class="line"></div>
-					<div class="row facilities">
-						<div class="col col-md-12">
-							<ul>
-								<li><i class="ti-time"></i> {data[0]?.duration}</li>
-								<li><i class="ti-user"></i> {data[0]?.occupancy}</li>
-								<li><i class="ti-location-pin"></i> {data[0]?.location}</li>
-							</ul>
+				<div class="col-md-4">
+					<div class="item" style="height: 95%;">
+						<div class="position-re o-hidden" style="height: 100%;">
+							<img
+								style="height: 100%; object-fit:cover"
+								src={data[1]?.images[0]?.path}
+								alt="images"
+							/>
+						</div>
+						<span class="category"
+							><a data-sveltekit-reload href="/paket-wisata/{data[1]?.slug}">{data[1]?.rate}</a
+							></span
+						>
+						<div class="con">
+							<h5>
+								<a data-sveltekit-reload href="/paket-wisata/{data[1]?.slug}">{data[1]?.name}</a>
+							</h5>
+							<div class="line"></div>
+							<div class="row facilities">
+								<div class="col col-md-12">
+									<ul>
+										<li><i class="ti-time"></i> {data[1]?.duration}</li>
+										<li><i class="ti-user"></i> {data[1]?.occupancy}</li>
+										<li><i class="ti-location-pin"></i> {data[1]?.location}</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item" style="height: 95%;">
+						<div class="position-re o-hidden" style="height: 100%;">
+							<img style="height: 100%;" src={data[2]?.images[0]?.path} alt="images" />
+						</div>
+						<span class="category"
+							><a data-sveltekit-reload href="/paket-wisata/{data[2]?.slug}">{data[2]?.rate}</a
+							></span
+						>
+						<div class="con">
+							<h5>
+								<a data-sveltekit-reload href="/paket-wisata/{data[0]?.slug}">{data[2]?.name}</a>
+							</h5>
+							<div class="line"></div>
+							<div class="row facilities">
+								<div class="col col-md-12">
+									<ul>
+										<li><i class="ti-time"></i> {data[2]?.duration}</li>
+										<li><i class="ti-user"></i> {data[2]?.occupancy}</li>
+										<li><i class="ti-location-pin"></i> {data[2]?.location}</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item" style="height: 95%;">
+						<div class="position-re o-hidden" style="height: 100%;">
+							<img style="height: 100%;" src={data[3]?.images[0]?.path} alt="images" />
+						</div>
+						<span class="category"
+							><a data-sveltekit-reload href="/paket-wisata/{data[3]?.slug}">{data[3]?.rate}</a
+							></span
+						>
+						<div class="con">
+							<h5>
+								<a data-sveltekit-reload href="/paket-wisata/{data[3]?.slug}">{data[3]?.name}</a>
+							</h5>
+							<div class="line"></div>
+							<div class="row facilities">
+								<div class="col col-md-12">
+									<ul>
+										<li><i class="ti-time"></i> {data[3]?.duration}</li>
+										<li><i class="ti-user"></i> {data[3]?.occupancy}</li>
+										<li><i class="ti-location-pin"></i> {data[3]?.location}</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="item" style="height: 95%;">
+						<div class="position-re o-hidden" style="height: 100%;">
+							<img style="height: 100%;" src={data[4]?.images[0]?.path} alt="images" />
+						</div>
+						<span class="category"
+							><a data-sveltekit-reload href="/paket-wisata/{data[4]?.slug}">{data[4]?.rate}</a
+							></span
+						>
+						<div class="con">
+							<h5>
+								<a data-sveltekit-reload href="/paket-wisata/{data[4]?.slug}">{data[4]?.name}</a>
+							</h5>
+							<div class="line"></div>
+							<div class="row facilities">
+								<div class="col col-md-12">
+									<ul>
+										<li><i class="ti-time"></i> {data[4]?.duration}</li>
+										<li><i class="ti-user"></i> {data[4]?.occupancy}</li>
+										<li><i class="ti-location-pin"></i> {data[4]?.location}</li>
+									</ul>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-		<div class="col-md-4">
-			<div class="item" style="height: 95%;">
-				<div class="position-re o-hidden" style="height: 100%;">
-					<img
-						style="height: 100%; object-fit:cover"
-						src={data[1]?.images[0]?.path}
-						alt="images"
-					/>
-				</div>
-				<span class="category"><a data-sveltekit-reload href="/paket-wisata/{data[1]?.slug}">{data[1]?.rate}</a></span>
-				<div class="con">
-					<h5><a data-sveltekit-reload href="/paket-wisata/{data[1]?.slug}">{data[1]?.name}</a></h5>
-					<div class="line"></div>
-					<div class="row facilities">
-						<div class="col col-md-12">
-							<ul>
-								<li><i class="ti-time"></i> {data[1]?.duration}</li>
-								<li><i class="ti-user"></i> {data[1]?.occupancy}</li>
-								<li><i class="ti-location-pin"></i> {data[1]?.location}</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4">
-			<div class="item" style="height: 95%;">
-				<div class="position-re o-hidden" style="height: 100%;">
-					<img style="height: 100%;" src={data[2]?.images[0]?.path} alt="images" />
-				</div>
-				<span class="category"><a data-sveltekit-reload href="/paket-wisata/{data[2]?.slug}">{data[2]?.rate}</a></span>
-				<div class="con">
-					<h5><a data-sveltekit-reload href="/paket-wisata/{data[0]?.slug}">{data[2]?.name}</a></h5>
-					<div class="line"></div>
-					<div class="row facilities">
-						<div class="col col-md-12">
-							<ul>
-								<li><i class="ti-time"></i> {data[2]?.duration}</li>
-								<li><i class="ti-user"></i> {data[2]?.occupancy}</li>
-								<li><i class="ti-location-pin"></i> {data[2]?.location}</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4">
-			<div class="item" style="height: 95%;">
-				<div class="position-re o-hidden" style="height: 100%;">
-					<img style="height: 100%;" src={data[3]?.images[0]?.path} alt="images" />
-				</div>
-				<span class="category"><a data-sveltekit-reload href="/paket-wisata/{data[3]?.slug}">{data[3]?.rate}</a></span>
-				<div class="con">
-					<h5><a data-sveltekit-reload href="/paket-wisata/{data[3]?.slug}">{data[3]?.name}</a></h5>
-					<div class="line"></div>
-					<div class="row facilities">
-						<div class="col col-md-12">
-							<ul>
-								<li><i class="ti-time"></i> {data[3]?.duration}</li>
-								<li><i class="ti-user"></i> {data[3]?.occupancy}</li>
-								<li><i class="ti-location-pin"></i> {data[3]?.location}</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4">
-			<div class="item" style="height: 95%;">
-				<div class="position-re o-hidden" style="height: 100%;">
-					<img style="height: 100%;" src={data[4]?.images[0]?.path} alt="images" />
-				</div>
-				<span class="category"><a data-sveltekit-reload href="/paket-wisata/{data[4]?.slug}">{data[4]?.rate}</a></span>
-				<div class="con">
-					<h5><a data-sveltekit-reload href="/paket-wisata/{data[4]?.slug}">{data[4]?.name}</a></h5>
-					<div class="line"></div>
-					<div class="row facilities">
-						<div class="col col-md-12">
-							<ul>
-								<li><i class="ti-time"></i> {data[4]?.duration}</li>
-								<li><i class="ti-user"></i> {data[4]?.occupancy}</li>
-								<li><i class="ti-location-pin"></i> {data[4]?.location}</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	{/if}
+		{/if}
 	</div>
 </section>
